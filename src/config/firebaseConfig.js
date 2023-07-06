32ef15774bf10ed533212a641b4adbe061ad4de0
karnan7 <adithkayalad@gmail.com>
2023-07-06 23:56:07 +0530

    deployed

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,9 +1,9 @@
 export default{
-    apiKey: "AIzaSyCC3fjKtKxMjtNk4XWHWZdXRSyviIWoW5U",
-    authDomain: "mygitapp-5f77b.firebaseapp.com",
-    projectId: "mygitapp-5f77b",
-    storageBucket: "mygitapp-5f77b.appspot.com",
-    messagingSenderId: "435784457875",
-    appId: "1:435784457875:web:f2fec4380b0208bce97766",
-    measurementId: "G-K2YD3M7CDL"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUREMENT_ID
   };
